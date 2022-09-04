9786f2801353d4d934738d55b91a928a1ff20b98
jpaulobf <jpaulobf@gmail.com>
2022-09-04 17:19:53 -0300

    Adjustments

---
+++ src/game/GameSprite.cs
@@ -0,0 +1,27 @@
+namespace game;
+
+using System.Drawing;
+
+public class GameSprite
+{
+    public Bitmap SpriteImage { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Width { get; set; }
+    public float Height { get; set; }
+    public int Velocity { get; set; }
+
+    public GameSprite()
+    {
+    }
+
+    public void Update(Graphics gfx, long timeframe) {
+        
+    }
+
+    public void Draw(Graphics gfx)
+    {
+        // Draw sprite image on screen
+        gfx.DrawImage(SpriteImage, X, Y, Width, Height);
+    }
+}
