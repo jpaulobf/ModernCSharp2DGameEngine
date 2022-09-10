2b93db369d41528f504dac19b7d2ea5ce1184c01
jpaulobf <jpaulobf@gmail.com>
2022-09-10 07:18:59 -0300

    WIP

--- src/game/EnemySprite.cs
+++ src/game/EnemySprite.cs
@@ -5,9 +5,10 @@ public class EnemySprite : GameSprite {
     /**
      * Enemy Sprite class
      */
-    public EnemySprite(string imageFilePath, int width, int height, int X, int Y, int velocity, byte tilesNumber, int millisecondsPerFrame) : base(imageFilePath, width, height, X, Y, velocity) {
+    public EnemySprite(string imageFilePath, int width, int height, int X, int Y, int velocity, byte tilesNumber = 1, int millisecondsPerFrame = 0) : base(imageFilePath, width, height, X, Y, velocity) {
         this.TilesNumber = tilesNumber;
         this.MillisecondsPerFrame = millisecondsPerFrame;
+        //this.RenderReversed = true;
     }
 
     /**
@@ -17,16 +18,25 @@ public class EnemySprite : GameSprite {
     {
         this.framecounter += timeframe;
 
-        if (this.framecounter < this.MillisecondsPerFrame * 10_000) {
-            StartX = 0;
-        } else {
-            StartX = (short)Width;
-            if (this.framecounter > this.MillisecondsPerFrame * 10_000 * 2) {
-                this.framecounter = 0;
+        if (this.TilesNumber > 1) {
+            if (this.framecounter < this.MillisecondsPerFrame * 10_000) {
+                this.StartX = 0;
+            } else {
+                this.StartX = (short)Width;
+                if (this.framecounter > this.MillisecondsPerFrame * 10_000 * 2) {
+                    this.framecounter = 0;
+                }
             }
+        } else {
+            this.StartX = 0;
         }
         
         this.SourceRect = new Rectangle(this.StartX, this.StartY, (short)this.Width, (short)this.Height);
         this.DestineRect = new Rectangle((short)this.X, (short)this.Y, (short)this.Width, (short)this.Height);
+
+        if (this.RenderReversed) {
+            this.SpriteImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            this.RenderReversed = false;
+        }
     }
 }
