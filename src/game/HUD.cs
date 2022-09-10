68f7b753f4a7a97c15184b1ae58fe7b9a99f5ae3
jpaulobf <jpaulobf@gmail.com>
2022-09-10 07:19:27 -0300

    new HUD class

---
+++ src/game/HUD.cs
@@ -0,0 +1,21 @@
+namespace game;
+
+public class HUD {
+
+    private Rectangle rectSep;
+    private Rectangle rect;
+
+    public HUD() {
+        this.rectSep = new Rectangle(0, 428, 738, 3);
+        this.rect = new Rectangle(0, 431, 738, 85);
+    }
+
+    public void Update(long frametime) {
+
+    }
+
+    public void Draw(Graphics gfx) {
+        gfx.FillRectangle(Brushes.Black, this.rectSep);
+        gfx.FillRectangle(new SolidBrush(Color.FromArgb(255, 144, 144, 144)), this.rect);
+    }
+}
