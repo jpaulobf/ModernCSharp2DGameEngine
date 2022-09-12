271f6fd5c7dbc4e6999a2d56071f553c56e33481
jpaulobf <jpaulobf@gmail.com>
2022-09-12 17:23:05 -0300

    render with primitives (WIP)

--- src/game/PlayerSprite.cs
+++ src/game/PlayerSprite.cs
@@ -3,12 +3,33 @@ namespace game;
 public class PlayerSprite : GameSprite {
 
     private GameInterface gameref;
+    private Bitmap LocalSpriteImage;
+    private SolidBrush yellow = new SolidBrush(Color.FromArgb(255, 232, 232, 74));
 
     /**
      * Player Sprite constructor
      */
     public PlayerSprite(GameInterface game, string imageFilePath, int width, int height, int X, int Y, int velocity) : base(imageFilePath, width, height, X, Y, velocity) {
         this.gameref = game;
+        this.LocalSpriteImage = new Bitmap(width, height);
+        this.Render();
+    }
+
+    private void Render() {
+        Graphics g = Graphics.FromImage(this.LocalSpriteImage);
+        g.FillRectangle(yellow, new Rectangle(13,0,6,6));
+        g.FillRectangle(yellow, new Rectangle(9,6,14,3));
+        g.FillRectangle(yellow, new Rectangle(4,9,24,2));
+        g.FillRectangle(yellow, new Rectangle(0,11,32,5));
+        g.FillRectangle(yellow, new Rectangle(0,16,9,2));
+        g.FillRectangle(yellow, new Rectangle(23,16,9,2));
+        g.FillRectangle(yellow, new Rectangle(0,18,4,2));
+        g.FillRectangle(yellow, new Rectangle(28,18,4,2));
+        g.FillRectangle(yellow, new Rectangle(13,16,6,14));
+        g.FillRectangle(yellow, new Rectangle(4,25,5,5));
+        g.FillRectangle(yellow, new Rectangle(23,25,5,5));
+        g.FillRectangle(yellow, new Rectangle(9,23,4,4));
+        g.FillRectangle(yellow, new Rectangle(19,23,4,4));
     }
 
     /**
@@ -25,6 +46,11 @@ public class PlayerSprite : GameSprite {
         }
 
         this.SourceRect = new Rectangle(StartX, StartY, (short)Width - 1, (short)Height);
+        //this.SourceRect = new Rectangle(0, 0, (short)Width, (short)Height);
         this.DestineRect = new Rectangle((short)X, (short)Y, (short)Width, (short)Height);
     }
+
+    //public void Draw(Graphics gfx) {
+    //    gfx.DrawImage(this.LocalSpriteImage, this.DestineRect, this.SourceRect, System.Drawing.GraphicsUnit.Pixel);
+   // }
 }
