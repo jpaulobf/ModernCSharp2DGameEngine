b0d5bbfa0e90b24bccb7c27d8f10ed5ec5f35fd6
jpaulobf <jpaulobf@gmail.com>
2022-09-18 11:42:04 -0300

    WIP

--- src/game/EnemySprite.cs
+++ src/game/EnemySprite.cs
@@ -1,12 +1,16 @@
 namespace game;
 
+using game.stages;
+
 public class EnemySprite : GameSprite {
 
     private GameInterface GameRef;
     protected long FrameCounter = 0;
-    protected short MaxLeft = 0;
-    protected short MaxRight = 0;
-    protected byte Direction = 0;
+    protected short MaxLeft     = 0;
+    protected short MaxRight    = 0;
+    protected byte Direction    = 0;
+    protected float CurrentX    = 0;
+    protected float StartY      = 0;
 
     /**
      * Enemy Sprite class
@@ -26,13 +30,14 @@ public class EnemySprite : GameSprite {
                        byte direction = 0) : base(imageFilePath, width, height, X, Y, velocity) {
         
         this.TilesNumber = tilesNumber;
-        this.MillisecondsPerTile = millisecondsPerTile;
+        this.MillisecsPerTile = millisecondsPerTile;
         this.GameRef = game;
         this.RenderReversed = reversed;
         this.FrameCounter = 0;
         this.MaxLeft = maxLeft;
         this.MaxRight = maxRight;
         this.Direction = direction;
+        this.CurrentX = X;
     }
 
     /**
@@ -43,16 +48,26 @@ public class EnemySprite : GameSprite {
         if (this.TilesNumber > 1) {            
             this.FrameCounter += timeframe;
 
-            if (this.FrameCounter < this.MillisecondsPerTile * 1_000) {
+            if (this.FrameCounter < this.MillisecsPerTile * 1_000) {
                 this.SourceStartX = 0;
             } else {
                 this.SourceStartX = (short)Width;
-                if (this.FrameCounter > this.MillisecondsPerTile * 1_000 * 2) {
+                if (this.FrameCounter > this.MillisecsPerTile * 1_000 * 2) {
                     this.FrameCounter = 0;
                 }
             }
         } else {
             this.SourceStartX = 0;
+        }
+
+        if (this.Direction == SpriteConstructor.LEFT) {
+            if (this.X > this.MaxLeft) {
+                this.X -= 1;
+            }
+        } else if (this.Direction == SpriteConstructor.RIGHT) {
+            if (this.X < this.MaxRight) {
+                this.X += 1;
+            }
         }
         
         this.SourceRect  = new Rectangle(this.SourceStartX, this.SourceStartY, (short)this.Width, (short)this.Height);
