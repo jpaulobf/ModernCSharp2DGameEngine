95f8b7ab09a113247c99059787cd5f6c00028ac4
jpaulobf <jpaulobf@gmail.com>
2022-09-20 09:41:32 -0300

    colision detection (WIP)

--- src/game/StaticSprite.cs
+++ src/game/StaticSprite.cs
@@ -2,13 +2,13 @@ namespace game;
 
 public class StaticSprite : GameSprite
 {
-    private GameInterface gameref;
+    private GameInterface GameRef;
 
     /**
      * Static Sprite constructor
      */
     public StaticSprite(GameInterface game, string imageFilePath, int width, int height, int X = 0, int Y = 0) : base(imageFilePath, width, height, X, Y, 0) {
-        this.gameref = game;
+        this.GameRef = game;
     }
 
     /**
@@ -17,5 +17,9 @@ public class StaticSprite : GameSprite
     public override void Update(long timeframe) {
         this.SourceRect = new Rectangle(0, 0, (short)this.Width, (short)this.Height);
         this.DestineRect = new Rectangle((short)this.X, (short)this.Y, (short)this.Width, (short)this.Height);
+
+        if (this.ColisionDetection(this.GameRef.GetPlayerSprite())) {
+            Console.WriteLine("Fuel...");
+        }
     }
 }
