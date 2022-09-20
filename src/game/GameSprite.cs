97662ee67ffdf9d6b33c86d9b463f3f885892e92
jpaulobf <jpaulobf@gmail.com>
2022-09-20 09:39:45 -0300

    colision detection (WIP)

--- src/game/GameSprite.cs
+++ src/game/GameSprite.cs
@@ -53,4 +53,9 @@ public abstract class GameSprite
         // Draw sprite image on screen
         gfx.DrawImage(this.SpriteImage, this.DestineRect, this.SourceRect, System.Drawing.GraphicsUnit.Pixel);
     }
+
+    public bool ColisionDetection(GameSprite othersprite) {
+        return (new Rectangle((short)this.X, (short)this.Y, (short)this.Width, (short)this.Height).IntersectsWith(
+                new Rectangle((short)othersprite.X, (short)othersprite.Y, (short)othersprite.Width, (short)othersprite.Height)));
+    }
 }
