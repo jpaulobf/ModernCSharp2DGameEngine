4b49b43d7faebd309fe687147e30c967e998f4ed
jpaulobf <jpaulobf@gmail.com>
2022-09-22 10:35:48 -0300

    Created the Utils file

---
+++ src/util/Util.cs
@@ -0,0 +1,31 @@
+namespace util;
+
+public class Util {
+    
+    // ParallelOptions options = new() { MaxDegreeOfParallelism = Environment.ProcessorCount };
+    // Util.ParallelWhile(options, () => this.isEngineRunning, body => { 
+    
+    public static void ParallelWhile(ParallelOptions parallelOptions,
+                                         Func<bool> condition,
+                                         Action<ParallelLoopState> body)
+    {
+        ArgumentNullException.ThrowIfNull(parallelOptions);
+        ArgumentNullException.ThrowIfNull(condition);
+        ArgumentNullException.ThrowIfNull(body);
+
+        int workersCount = parallelOptions.MaxDegreeOfParallelism switch
+        {
+            -1 => Int32.MaxValue, // -1 means unlimited parallelism.
+            _ => parallelOptions.MaxDegreeOfParallelism
+        };
+
+        Parallel.For(0, workersCount, parallelOptions, (_, state) =>
+        {
+            while (!state.ShouldExitCurrentIteration)
+            {
+                if (!condition()) { state.Stop(); break; }
+                body(state);
+            }
+        });
+    }
+}
