ae13e2b571c6631a611451b5e29bd1971f2996de
jpaulobf <jpaulobf@gmail.com>
2022-09-26 11:39:06 -0300

    Reset (WIP)

--- src/game/PlayerSprite.cs
+++ src/game/PlayerSprite.cs
@@ -5,18 +5,20 @@ public class PlayerSprite : GameSprite {
     private GameInterface GameRef;
     private Bitmap LocalSpriteImage;
     private Bitmap SpriteSplosion;
-    private SolidBrush YellowBrush = new SolidBrush(Color.FromArgb(255, 232, 232, 74));
-    private bool Coliding = false;
+    private float OgWidth           = 0;
+    private float OgHeight          = 0;
+    private SolidBrush YellowBrush  = new SolidBrush(Color.FromArgb(255, 232, 232, 74));
+    private bool Coliding           = false;
 
     /**
      * Player Sprite constructor
      */
     public PlayerSprite(GameInterface game, string imageFilePath, int width, int height, int X, int Y, int velocity) : base(imageFilePath, width, height, X, Y, velocity) {
-        this.GameRef = game;
-        string splosionPath = "img\\sprite_splosion.png";
-        this.SpriteSplosion = new Bitmap(@splosionPath);
-        this.LocalSpriteImage = new Bitmap(width, height);
-        // this.Render();
+        this.GameRef            = game;
+        this.SpriteSplosion     = new Bitmap(@"img\\sprite_splosion.png");
+        this.LocalSpriteImage   = new Bitmap(@imageFilePath);
+        this.OgWidth            = width;
+        this.OgHeight           = height;
     }
 
     public void SetColision() {
@@ -27,27 +29,9 @@ public class PlayerSprite : GameSprite {
     /**
      * Animate the sprite colision
      */
-    private void AnimateExplosion()
-    {
-        
-    }
-
-    private void Render() {
-        Graphics g = Graphics.FromImage(this.LocalSpriteImage);
-        g.FillRectangle(YellowBrush, new Rectangle(13,0,6,6));
-        g.FillRectangle(YellowBrush, new Rectangle(9,6,14,3));
-        g.FillRectangle(YellowBrush, new Rectangle(4,9,24,2));
-        g.FillRectangle(YellowBrush, new Rectangle(0,11,32,5));
-        g.FillRectangle(YellowBrush, new Rectangle(0,16,9,2));
-        g.FillRectangle(YellowBrush, new Rectangle(23,16,9,2));
-        g.FillRectangle(YellowBrush, new Rectangle(0,18,4,2));
-        g.FillRectangle(YellowBrush, new Rectangle(28,18,4,2));
-        g.FillRectangle(YellowBrush, new Rectangle(13,16,6,14));
-        g.FillRectangle(YellowBrush, new Rectangle(4,25,5,5));
-        g.FillRectangle(YellowBrush, new Rectangle(23,25,5,5));
-        g.FillRectangle(YellowBrush, new Rectangle(9,23,4,4));
-        g.FillRectangle(YellowBrush, new Rectangle(19,23,4,4));
-    }
+    private void AnimateExplosion() {
+        //TODO
+    }   
 
     /**
      * Player Sprite update method
@@ -55,8 +39,7 @@ public class PlayerSprite : GameSprite {
     public override void Update(long timeframe) {
         if (!this.Coliding) {
             if (!this.Lefting && !this.Righting) {
-                //Default
-                this.SourceStartX = (short)Width;
+                this.SourceStartX = (short)Width; //Default
             } else if (this.Lefting) { //Lefting
                 this.SourceStartX = 0;
             } else if (this.Righting) { //Righting
@@ -69,7 +52,18 @@ public class PlayerSprite : GameSprite {
             this.SpriteImage = this.SpriteSplosion;
         }
 
+        //define the source & destine rect
         this.SourceRect = new Rectangle(SourceStartX, SourceStartY, (short)Width - 1, (short)Height);
         this.DestineRect = new Rectangle((short)X, (short)Y, (short)Width, (short)Height);
+    }
+
+    /**
+     * Reset player sprite
+     */
+    internal void Reset() {
+        this.SpriteImage    = this.LocalSpriteImage;
+        this.Coliding       = false;
+        this.Width          = this.OgWidth;
+        this.Height         = this.OgHeight;
     }
 }
