77853a819ebe7d84b7fb08c7a0745375bd18f21c
jpaulobf <jpaulobf@gmail.com>
2022-09-29 11:41:24 -0300

    Code adjusted

--- src/game/EnemySprite.cs
+++ src/game/EnemySprite.cs
@@ -1,8 +1,10 @@
 namespace game;
 
-using System;
 using game.stages;
 
+/**
+ * Class representing enemies sprites
+ */
 public class EnemySprite : GameSprite {
 
     private GameInterface GameRef;
@@ -12,7 +14,7 @@ public class EnemySprite : GameSprite {
     protected byte Direction    = 0;
 
     /**
-     * Enemy Sprite class
+     * Enemy Sprite constructor
      */
     public EnemySprite(GameInterface game, 
                        string imageFilePath, 
@@ -27,15 +29,15 @@ public class EnemySprite : GameSprite {
                        short maxLeft = 0,
                        short maxRight = 0, 
                        byte direction = 0) : base(imageFilePath, width, height, X, Y, velocity) {
-        
-        this.TilesNumber = tilesNumber;
-        this.MillisecsPerTile = millisecondsPerTile;
-        this.GameRef = game;
-        this.RenderReversed = reversed;
-        this.FrameCounter = 0;
-        this.MaxLeft = maxLeft;
-        this.MaxRight = maxRight;
-        this.Direction = direction;
+        //after base constructor
+        this.TilesNumber        = tilesNumber;
+        this.MillisecsPerTile   = millisecondsPerTile;
+        this.GameRef            = game;
+        this.RenderReversed     = reversed;
+        this.FrameCounter       = 0;
+        this.MaxLeft            = maxLeft;
+        this.MaxRight           = maxRight;
+        this.Direction          = direction;
     }
 
     /**
@@ -43,34 +45,52 @@ public class EnemySprite : GameSprite {
      */
     public override void Update(long frametime)
     {
-        if (this.TilesNumber > 1) {            
+        //if the current sprite has more than 1 tile
+        if (this.TilesNumber > 1)
+        {            
             this.FrameCounter += frametime;
-            if (this.FrameCounter < this.MillisecsPerTile * 10_000) {
+            if (this.FrameCounter < this.MillisecsPerTile * 10_000) 
+            {
                 this.SourceStartX = 0;
-            } else {
+            } 
+            else 
+            {
                 this.SourceStartX = (short)Width;
-                if (this.FrameCounter > this.MillisecsPerTile * 10_000 * 2) {
+                if (this.FrameCounter > this.MillisecsPerTile * 10_000 * 2) 
+                {
                     this.FrameCounter = 0;
                 }
             }
-        } else {
+        } 
+        else 
+        {
             this.FrameCounter = 0;
             this.SourceStartX = 0;
         }
         
-        if (this.Direction != 0) {
+        if (this.Direction != 0) 
+        {
             float step = (float)(this.Velocity * ((double)frametime / 10_000_000));
-            if (this.Direction == SpriteConstructor.LEFT) {
-                if (this.X > this.MaxLeft) {
+            if (this.Direction == SpriteConstructor.LEFT) 
+            {
+                if (this.X > this.MaxLeft) 
+                {
                     this.X -= step;
-                } else {
+                } 
+                else 
+                {
                     this.Direction = SpriteConstructor.RIGHT;
                     this.FlipX();
                 }
-            } else if (this.Direction == SpriteConstructor.RIGHT) {
-                if (this.X < (this.MaxRight - this.Width-1)) {
+            } 
+            else if (this.Direction == SpriteConstructor.RIGHT) 
+            {
+                if (this.X < (this.MaxRight - this.Width-1)) 
+                {
                     this.X += step;
-                } else {
+                } 
+                else 
+                {
                     this.Direction = SpriteConstructor.LEFT;
                     this.FlipX();
                 }
@@ -82,7 +102,8 @@ public class EnemySprite : GameSprite {
         this.DestineRect = new Rectangle((short)this.X, (short)this.Y, (short)this.Width, (short)this.Height);
 
         //verify if the player sprite is coliding with this current
-        if (this.CollisionDetection(this.GameRef.GetPlayerSprite())) {
+        if (this.CollisionDetection(this.GameRef.GetPlayerSprite())) 
+        {
             this.TilesNumber = 0;
             this.Direction = 0;
             this.GameRef.GetPlayerSprite().SetCollision();
