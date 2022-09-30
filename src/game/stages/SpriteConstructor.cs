51dc90131b099edb97050358fe1eb380e0a9b874
jpaulobf <jpaulobf@gmail.com>
2022-09-30 10:50:22 -0300

    Defines the type for the Enemy Sprite

--- src/game/stages/SpriteConstructor.cs
+++ src/game/stages/SpriteConstructor.cs
@@ -4,22 +4,22 @@ public class SpriteConstructor {
     private GameInterface gameRef;
     public int X { get; set; }
     public int Y { get; set; }
-    public int type { get; set; }
+    public byte type { get; set; }
     public int parameter { get; set; }
     public bool flag { get; set; }
     private GameSprite gamesprite;
     public static byte HOUSE = 1;
     public static byte HOUSE2 = 2;
     public static byte FUEL = 3;
-    public static byte SHIP = 4;
-    public static byte HELI = 5;
+    public static byte HELI = 4;
+    public static byte SHIP = 5;
     public static byte LEFT = 1;
     public static byte RIGHT = 2;
 
     /**
      * Constructor
      */
-    public SpriteConstructor(GameInterface game, int type, int X, byte parameter = 1, bool flag = false, short maxLeft = 0, short maxRight = 0, byte direction = 0) {
+    public SpriteConstructor(GameInterface game, byte type, int X, byte parameter = 1, bool flag = false, short maxLeft = 0, short maxRight = 0, byte direction = 0) {
         this.gameRef = game;
         this.X = X;
         this.Y = Y;
@@ -33,22 +33,22 @@ public class SpriteConstructor {
         } else if (type == FUEL) {
             this.gamesprite = new StaticSprite(game, "img\\fuel.png", 32, 55, X);
         } else if (type == SHIP) {
-            this.gamesprite = new EnemySprite(game, "img\\ship.png", 73, 18, X, 0, 100, parameter, 25, flag, maxLeft, maxRight, direction);
+            this.gamesprite = new EnemySprite(game, type, "img\\ship.png", 73, 18, X, 0, 100, parameter, 25, flag, maxLeft, maxRight, direction);
         } else {
-            this.gamesprite = new EnemySprite(game, "img\\helitile.png", 36, 23, X, 0, 100, parameter, 25, flag, maxLeft, maxRight, direction);
+            this.gamesprite = new EnemySprite(game, type, "img\\helitile.png", 36, 23, X, 0, 100, parameter, 25, flag, maxLeft, maxRight, direction);
         }
     }
 
-    public void Render(Graphics gfx, long frametime, int currentLineYPosition, int offset, int Y) {
-        this.Update(frametime, currentLineYPosition, offset, Y);
+    public void Render(Graphics gfx, long frametime, int currentLineYPosition, int offset, int Y, bool colliding) {
+        this.Update(frametime, currentLineYPosition, offset, Y, colliding);
         this.Draw(gfx);
 
     }
 
-    private void Update(long frametime, int currentLineYPosition, int offset, int Y) {
+    private void Update(long frametime, int currentLineYPosition, int offset, int Y, bool colliding) {
         this.gamesprite.Y = Y - currentLineYPosition + offset;
         this.gamesprite.RenderReversed = this.flag;
-        this.gamesprite.Update(frametime);
+        this.gamesprite.Update(frametime, colliding);
     }
 
     /**
