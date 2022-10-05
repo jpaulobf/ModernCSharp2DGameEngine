8de31d5512882e91a2cce45d5f0611edc4d95d33
jpaulobf <jpaulobf@gmail.com>
2022-10-05 12:08:43 -0300

    Code cleaning

--- src/engine/CanvasEngineInterface.cs
+++ src/engine/CanvasEngineInterface.cs
@@ -5,15 +5,21 @@ public interface CanvasEngineInterface {
      * Update the game logic / receives the frametime
      * @param frametime
      */
-    public void update(long frametime);
+    public void Update(long frametime);
 
     /**
      * Draw the game / receives the frametime
      * @param frametime
      */
-    public void draw(long frametime);
+    public void Draw(long frametime);
 
+    /**
+     * Render the backbuffer in the canvas
+     */
     public void Render();
     
+    /**
+     * Dispose the graphics
+     */
     void GraphicDispose();
 }
