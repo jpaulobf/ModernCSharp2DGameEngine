2bc97c194273bcbf2b3a99e4b7ca111ab732797c
jpaulobf <jpaulobf@gmail.com>
2022-10-05 12:09:07 -0300

    Code cleaning

--- src/game/stages/SpriteConstructor.cs
+++ src/game/stages/SpriteConstructor.cs
@@ -1,13 +1,13 @@
 namespace game.stages;
 
 public class SpriteConstructor {
-    private GameInterface gameRef;
+    private GameInterface GameRef;
     public int X { get; set; }
     public int Y { get; set; }
-    public byte type { get; set; }
-    public int parameter { get; set; }
-    public bool flag { get; set; }
-    private GameSprite gamesprite;
+    public byte Type { get; set; }
+    public int Parameter { get; set; }
+    public bool Flag { get; set; }
+    private GameSprite GameSprite;
     public static byte HOUSE = 1;
     public static byte HOUSE2 = 2;
     public static byte FUEL = 3;
@@ -20,22 +20,22 @@ public class SpriteConstructor {
      * Constructor
      */
     public SpriteConstructor(GameInterface game, byte type, int X, byte parameter = 1, bool flag = false, short maxLeft = 0, short maxRight = 0, byte direction = 0) {
-        this.gameRef = game;
+        this.GameRef = game;
         this.X = X;
         this.Y = Y;
-        this.type = type;
-        this.parameter = parameter;
-        this.flag = flag;
+        this.Type = type;
+        this.Parameter = parameter;
+        this.Flag = flag;
         if (type == HOUSE) {
-            this.gamesprite = new StaticSprite(game, "img\\house.png", 73, 44, X);
+            this.GameSprite = new StaticSprite(game, "img\\house.png", 73, 44, X);
         } else if (type == HOUSE2) {
-            this.gamesprite = new StaticSprite(game, "img\\house2.png", 73, 44, X);
+            this.GameSprite = new StaticSprite(game, "img\\house2.png", 73, 44, X);
         } else if (type == FUEL) {
-            this.gamesprite = new StaticSprite(game, "img\\fuel.png", 32, 55, X);
+            this.GameSprite = new StaticSprite(game, "img\\fuel.png", 32, 55, X);
         } else if (type == SHIP) {
-            this.gamesprite = new EnemySprite(game, type, "img\\ship.png", 73, 18, X, 0, 100, parameter, 25, flag, maxLeft, maxRight, direction);
+            this.GameSprite = new EnemySprite(game, type, "img\\ship.png", 73, 18, X, 0, 100, parameter, 25, flag, maxLeft, maxRight, direction);
         } else {
-            this.gamesprite = new EnemySprite(game, type, "img\\helitile.png", 36, 23, X, 0, 100, parameter, 25, flag, maxLeft, maxRight, direction);
+            this.GameSprite = new EnemySprite(game, type, "img\\helitile.png", 36, 23, X, 0, 100, parameter, 25, flag, maxLeft, maxRight, direction);
         }
     }
 
@@ -45,19 +45,19 @@ public class SpriteConstructor {
     }
 
     public void Reset() {
-        this.gamesprite.Reset();
+        this.GameSprite.Reset();
     }
 
     internal void Update(long frametime, int currentLineYPosition, int offset, int Y, bool colliding) {
-        this.gamesprite.Y = Y - currentLineYPosition + offset;
-        this.gamesprite.RenderReversed = this.flag;
-        this.gamesprite.Update(frametime, colliding);
+        this.GameSprite.Y = Y - currentLineYPosition + offset;
+        this.GameSprite.RenderReversed = this.Flag;
+        this.GameSprite.Update(frametime, colliding);
     }
 
     /**
      * Render method
      */
     internal void Draw(Graphics gfx) {
-        this.gamesprite.Draw(gfx);
+        this.GameSprite.Draw(gfx);
     }
 }
