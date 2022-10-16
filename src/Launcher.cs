2de77d975e2758abcfa1cadeb3cc0006bf31f429
jpaulobf <jpaulobf@gmail.com>
2022-10-16 14:46:40 -0300

    new launcher

---
+++ src/Launcher.cs
@@ -0,0 +1,25 @@
+using Engine;
+
+namespace GameEngine;
+
+/**
+ * This is the Launcher to the program
+ */
+static class Launcher
+{
+    //const
+    private const string VISUAL_STUDIO = "..\\..\\..\\";
+    private const string VS_CODE = ".\\";
+    
+    //current gamepath
+    public static string path = VS_CODE;
+    
+    /**
+     * Just start the "Game" class, passing the desired FPS
+     * 0 for unlimited
+     */
+    static void Main()
+    {
+        new MyGame(0);
+    } 
+}
