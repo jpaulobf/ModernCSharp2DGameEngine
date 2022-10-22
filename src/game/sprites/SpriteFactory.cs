73a6f77056fa185b1da6b1bf8640a369b7cdfb89
jpaulobf <jpaulobf@gmail.com>
2022-10-22 14:23:23 -0300

    LoadStuffs implemented

--- src/game/sprites/SpriteFactory.cs
+++ src/game/sprites/SpriteFactory.cs
@@ -1,5 +1,7 @@
 namespace Game;
 
+using Util;
+
 /**
  * Author: Joao P B Faria
  * Date: Oct/2022
@@ -9,16 +11,25 @@ public class SpriteFactory
 {
     public static GameSprite CreateSprite(IGame game, byte type, int x, int y, byte parameter = 1, bool flag = false, short maxLeft = 0, short maxRight = 0, byte direction = 0)
     {
-        if (type == GameSprite.HOUSE) {
-            return (new StaticSprite(game, "img\\house.png", 73, 44, GameSprite.HOUSE, x, y));
-        } else if (type == GameSprite.HOUSE2) {
-            return (new StaticSprite(game, "img\\house2.png", 73, 44, GameSprite.HOUSE2, x, y));
-        } else if (type == GameSprite.FUEL) {
-            return (new StaticSprite(game, "img\\fuel.png", 32, 55, GameSprite.FUEL, x, y));
-        } else if (type == GameSprite.SHIP) {
-            return (new EnemySprite(game, type, "img\\ship.png", 73, 18, x, y, 100, parameter, 25, flag, maxLeft, maxRight, direction));
-        } else {
-            return (new EnemySprite(game, type, "img\\helitile.png", 36, 23, x, y, 100, parameter, 25, flag, maxLeft, maxRight, direction));
+        if (type == GameSprite.HOUSE) 
+        {
+            return (new StaticSprite(game, LoadingStuffs.GetInstance().GetImage("house-1"), 73, 44, GameSprite.HOUSE, x, y));
+        } 
+        else if (type == GameSprite.HOUSE2) 
+        {
+            return (new StaticSprite(game, LoadingStuffs.GetInstance().GetImage("house-2"), 73, 44, GameSprite.HOUSE2, x, y));
+        } 
+        else if (type == GameSprite.FUEL) 
+        {
+            return (new StaticSprite(game, LoadingStuffs.GetInstance().GetImage("fuel"), 32, 55, GameSprite.FUEL, x, y));
+        } 
+        else if (type == GameSprite.SHIP) 
+        {
+            return (new EnemySprite(game, type, LoadingStuffs.GetInstance().GetImage("ship"), 73, 18, x, y, 100, parameter, 25, flag, maxLeft, maxRight, direction));
+        } 
+        else 
+        {
+            return (new EnemySprite(game, type, LoadingStuffs.GetInstance().GetImage("heli-tile"), 36, 23, x, y, 100, parameter, 25, flag, maxLeft, maxRight, direction));
         }
     }
 }
