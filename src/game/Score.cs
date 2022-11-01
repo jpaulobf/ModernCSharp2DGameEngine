2b47f3dedae24a427bfe0be6850975cbc15c5e95
jpaulobf <jpaulobf@gmail.com>
2022-11-01 11:17:27 -0300

    Score (WIP)

--- src/game/Score.cs
+++ src/game/Score.cs
@@ -3,11 +3,15 @@ using Game;
 public class Score
 {
     private IGame GameRef;
-    private const short HELI_POINTS     = 100;
-    private const short SHIP_POINTS     = 100;
-    private const short FUEL_POINTS     = 100;
-    private const short AIRPLANE_POINTS = 100;
-    private const short BRIDGE_POINTS   = 1000;
+    private const short HELI_POINTS         = 100;
+    private const short SHIP_POINTS         = 100;
+    private const short FUEL_POINTS         = 100;
+    private const short AIRPLANE_POINTS     = 100;
+    private const short BRIDGE_POINTS       = 1000;
+    private int Points                      = 0;
+    private string StPoints                 = "";
+    private const short RightPosX           = 0;
+    private const short TopPosY             = 0;
 
     public Score(IGame gameRef)
     {
@@ -21,11 +25,17 @@ public class Score
 
     public void Update(long frametime)
     {
-
+        this.StPoints = Points.ToString();
     }
 
     public void Draw(Graphics gfx)
     {
 
     }
+
+    public void Reset()
+    {
+        this.Points = 0;
+    }
+
 }
