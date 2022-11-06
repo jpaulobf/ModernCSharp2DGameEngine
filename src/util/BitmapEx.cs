64f9a00a6b0e9d087a297588cd1fb1f6c47ab8e3
jpaulobf <jpaulobf@gmail.com>
2022-11-06 12:08:08 -0300

    Code cleaning

--- src/util/BitmapEx.cs
+++ src/util/BitmapEx.cs
@@ -2,11 +2,18 @@ namespace Util;
 
 using GameEngine;
 
+/**
+ * Author:      Joao Paulo B Faria
+ * Date:        Oct/2022
+ * Description: Class utility to create a bitmap with the current system path
+ */
 public class BitmapEx
 {
+    //no public constructor
     private BitmapEx() 
     {}
 
+    //method factory
     public static Bitmap New(string filepath) 
     {
         return new Bitmap(@"" + Launcher.path + filepath);
