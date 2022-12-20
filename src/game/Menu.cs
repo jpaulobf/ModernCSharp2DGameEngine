2a903d304a6d5ef44806a715154c0b034ec2cd0c
jpaulobf <jpaulobf@gmail.com>
2022-12-20 19:11:16 -0300

    Menu (WIP)

--- src/game/Menu.cs
+++ src/game/Menu.cs
@@ -7,13 +7,19 @@ public class Menu
     private IGame GameRef;
     private Bitmap MainLogo;
     private Bitmap Selector;
-    private int MainLogoX = 372;
-    private int MainLogoY = 40;
-
-    private int SelectorX = 78;
-    private int SelectorY = 484;
-
-
+    private Bitmap LabelStart;
+    private Bitmap LabelOptions;
+    private Bitmap LabelExit;
+    private int MainLogoX       = 372;
+    private int MainLogoY       = 40;
+    private int SelectorX       = 78;
+    private int SelectorY       = 484;
+    private int LabelStartX     = 545;
+    private int LabelStartY     = 498;
+    private int LabelOptionsX   = 611;
+    private int LabelOptionsY   = 574;
+    private int LabelExitX      = 592;
+    private int LabelExitY      = 650;
 
     /**
      * Constructor
@@ -22,6 +28,10 @@ public class Menu
     {
         this.GameRef        = game;
         this.MainLogo       = LoadingStuffs.GetInstance().GetImage("main-logo");
+        this.Selector       = LoadingStuffs.GetInstance().GetImage("selector");
+        this.LabelStart     = LoadingStuffs.GetInstance().GetImage("label-start");
+        this.LabelOptions   = LoadingStuffs.GetInstance().GetImage("label-options");
+        this.LabelExit      = LoadingStuffs.GetInstance().GetImage("label-exit");
     }
 
     /**
@@ -39,7 +49,10 @@ public class Menu
     {
         gfx.FillRectangle(new SolidBrush(Color.FromArgb(255, 58, 80, 74)), 0, 0, GameRef.GetInternalResolutionWidth(), GameRef.GetInternalResolutionHeight());
         gfx.DrawImage(this.MainLogo, this.MainLogoX, this.MainLogoY, this.MainLogo.Width, this.MainLogo.Height);
-        //TODO
+        gfx.DrawImage(this.Selector, this.SelectorX, this.SelectorY, this.Selector.Width, this.Selector.Height);
+        gfx.DrawImage(this.LabelStart, this.LabelStartX, this.LabelStartY, this.LabelStart.Width, this.LabelStart.Height);
+        gfx.DrawImage(this.LabelOptions, this.LabelOptionsX, this.LabelOptionsY, this.LabelOptions.Width, this.LabelOptions.Height);
+        gfx.DrawImage(this.LabelExit, this.LabelExitX, this.LabelExitY, this.LabelExit.Width, this.LabelExit.Height);
     }
 
     /**
