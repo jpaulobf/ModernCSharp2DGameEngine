674cba0d6ca80212eae0996888d60a1b054055b5
jpaulobf <jpaulobf@gmail.com>
2022-12-26 23:04:35 -0300

    Options Menu (WIP)

--- src/game/Options.cs
+++ src/game/Options.cs
@@ -3,13 +3,14 @@ namespace Game;
 public class Options
 {
     private IGame GameRef;
+    private byte currentPosition = 0;
 
     /**
      * Constructor
      */
     public Options(IGame game) 
     {
-        this.GameRef        = game;
+        this.GameRef = game;
     }
 
     /**
@@ -25,7 +26,7 @@ public class Options
      */
     public void Draw(Graphics gfx) 
     {
-        //gfx.FillRectangle(Brushes.Black, this.SeparatorRect);
+        gfx.FillRectangle(new SolidBrush(Color.FromArgb(255, 58, 80, 74)), 0, 0, GameRef.GetInternalResolutionWidth(), GameRef.GetInternalResolutionHeight());
         //gfx.DrawImage(this.FuelMeter, this.FuelMeterX, this.FuelMeterY, this.FuelMeter.Width, this.FuelMeter.Height);
         //TODO
     }
@@ -35,5 +36,40 @@ public class Options
      */
     internal void Reset()
     {
+    }
+
+    public void KeyUp(object? sender, KeyEventArgs e)
+    {
+        if (e.KeyValue == 38) //up
+        {
+            if (this.currentPosition == 0)
+            {
+                this.currentPosition = 2;
+            }
+            else 
+            {
+                this.currentPosition--;
+            }
+        }
+        else if (e.KeyValue == 40) //down
+        {
+            if (this.currentPosition == 2)
+            {
+                this.currentPosition = 0;
+            }
+            else 
+            {
+                this.currentPosition++;
+            }
+        }
+        else if (e.KeyValue == 32 || e.KeyValue == 13) //space or enter
+        {
+            //todo
+        } 
+        else if (e.KeyValue == 27) //ESC
+        {
+            //back to menu
+            this.GameRef.SetGameStateToMenu();
+        }
     }
 }
