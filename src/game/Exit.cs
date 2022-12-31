e9cf490eea1695c528507998dd2845697d70b252
jpaulobf <jpaulobf@gmail.com>
2022-12-30 22:12:53 -0300

    Exit screen (WIP)

--- src/game/Exit.cs
+++ src/game/Exit.cs
@@ -6,16 +6,40 @@ public class Exit
 {
     private IGame GameRef;
     private byte currentPosition    = 0;
-    private int x = 0;
-    private int y = 0;
+    private short ExitWindowX = 0;
+    private short ExitWindowY = 0;
+    private short ReallyX = 0;
+    private short ReallyY = 0;
+    private short YesX = 0;
+    private short YesY = 0;
+    private short NoX = 0;
+    private short NoY = 0;
+    private short YesBtX = 0;
+    private short YesBtY = 0;
+    private short NoBtX = 0;
+    private short NoBtY = 0;
+    private Bitmap ReallyBitmap = Util.LoadingStuffs.GetInstance().GetImage("really");
+    private Bitmap YesBitmap    = Util.LoadingStuffs.GetInstance().GetImage("bt-yes");
+    private Bitmap NoBitmap    = Util.LoadingStuffs.GetInstance().GetImage("bt-no");
+
     /**
      * Constructor
      */
     public Exit(IGame game) 
     {
         this.GameRef = game;
-        this.x = (game.GetInternalResolutionWidth() / 2) - 120;
-        this.y = (game.GetInternalResolutionHeight() / 2) - 80;
+        this.ExitWindowX    = (short)((game.GetInternalResolutionWidth() / 2) - 120);
+        this.ExitWindowY    = (short)((game.GetInternalResolutionHeight() / 2) - 80);
+        this.ReallyX        = (short)(this.ExitWindowX + 20);
+        this.ReallyY        = (short)(this.ExitWindowY + 20);
+        this.YesX           = (short)(this.ExitWindowX + 30);
+        this.YesY           = (short)(this.ExitWindowY + 60);
+        this.NoX            = (short)(this.ExitWindowX + 130);
+        this.NoY            = (short)(this.ExitWindowY + 60);
+        this.YesBtX         = (short)(this.YesX + 18);
+        this.YesBtY         = (short)(this.YesY + 8);
+        this.NoBtX          = (short)(this.NoX + 24);
+        this.NoBtY          = (short)(this.NoY + 8);
     }
 
     /**
@@ -30,8 +54,16 @@ public class Exit
      */
     public void Draw(Graphics gfx) 
     {
-        gfx.FillRectangle(new SolidBrush(Color.FromArgb(255, 200, 200, 190)), x, y, 240, 120);
-        gfx.DrawRectangle(new Pen(Color.FromArgb(255, 0, 0, 0)), x, y, 240, 120);
+        gfx.FillRectangle(new SolidBrush(Color.FromArgb(255, 200, 200, 190)), ExitWindowX, ExitWindowY, 240, 120);
+        gfx.DrawRectangle(new Pen(Color.FromArgb(255, 0, 0, 0)), ExitWindowX, ExitWindowY, 240, 120);
+        gfx.DrawImage(ReallyBitmap, ReallyX, ReallyY, ReallyBitmap.Width, ReallyBitmap.Height);
+        gfx.FillRectangle(new SolidBrush(Color.FromArgb(255, 170, 170, 170)), YesX, YesY, 80, 30);
+        gfx.FillRectangle(new SolidBrush(Color.FromArgb(255, 170, 170, 170)), NoX, NoY, 80, 30);
+        gfx.DrawRectangle(new Pen(Color.FromArgb(255, 0, 0, 0)), YesX, YesY, 80, 30);
+        gfx.DrawRectangle(new Pen(Color.FromArgb(255, 0, 0, 0)), NoX, NoY, 80, 30);
+
+        gfx.DrawImage(YesBitmap, YesBtX, YesBtY, YesBitmap.Width, YesBitmap.Height);
+        gfx.DrawImage(NoBitmap, NoBtX, NoBtY, NoBitmap.Width, NoBitmap.Height);
     }
 
     /**
