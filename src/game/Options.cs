a612277171fd7f44bb9f927b1ef4883ec3596450
jpaulobf <jpaulobf@gmail.com>
2023-01-14 18:22:37 -0300

    Options (WIP)

--- src/game/Options.cs
+++ src/game/Options.cs
@@ -7,24 +7,31 @@ public class Options
     private IGame GameRef;
     private Bitmap Selector;
     private Bitmap LabelPlayMusic;
-
-
+    private Bitmap LabelPlaySFX;
+    private Bitmap LabelExitOptions;
     private byte CurrentPosition    = 0;
-    private int SelectorX           = 66;
+    private const int OgSelectorX   = 66;
+    private int SelectorX           = OgSelectorX;
     private const int SelectorYBase = 106;
     private const int SelectorYDiff = 70;
     private int SelectorY           = SelectorYBase;
     private int LabelPlayMusicX     = 128;
     private int LabelPlayMusicY     = 120;
+    private int LabelPlaySFXX       = 128;
+    private int LabelPlaySFXY       = 190;
+    private int LabelExitOptionsX   = 38;
+    private int LabelExitOptionsY   = 627;
 
     /**
      * Constructor
      */
     public Options(IGame game) 
     {
-        this.GameRef        = game;
-        this.Selector       = LoadingStuffs.GetInstance().GetImage("selector");
-        this.LabelPlayMusic = LoadingStuffs.GetInstance().GetImage("label-play-music");
+        this.GameRef            = game;
+        this.Selector           = LoadingStuffs.GetInstance().GetImage("selector");
+        this.LabelPlayMusic     = LoadingStuffs.GetInstance().GetImage("label-play-music");
+        this.LabelPlaySFX       = LoadingStuffs.GetInstance().GetImage("label-play-sfx");
+        this.LabelExitOptions   = LoadingStuffs.GetInstance().GetImage("label-exit-options");
     }
 
     /**
@@ -40,6 +47,8 @@ public class Options
         gfx.FillRectangle(new SolidBrush(Color.FromArgb(255, 58, 80, 74)), 0, 0, GameRef.GetInternalResolutionWidth(), GameRef.GetInternalResolutionHeight());
         gfx.DrawImage(this.Selector, this.SelectorX, this.SelectorY, this.Selector.Width, this.Selector.Height);
         gfx.DrawImage(this.LabelPlayMusic, this.LabelPlayMusicX, this.LabelPlayMusicY, this.LabelPlayMusic.Width, this.LabelPlayMusic.Height);
+        gfx.DrawImage(this.LabelPlaySFX, this.LabelPlaySFXX, this.LabelPlaySFXY, this.LabelPlaySFX.Width, this.LabelPlaySFX.Height);
+        gfx.DrawImage(this.LabelExitOptions, this.LabelExitOptionsX, this.LabelExitOptionsY, this.LabelExitOptions.Width, this.LabelExitOptions.Height);
 
 
     }
@@ -53,6 +62,9 @@ public class Options
 
     public void KeyUp(object? sender, KeyEventArgs e)
     {
+        int diff = 0;
+        int diffY = 0;
+
         if (e.KeyValue == 38) //up
         {
             if (this.CurrentPosition == 0)
@@ -85,6 +97,13 @@ public class Options
             this.GameRef.SetGameStateToMenu();
         }
 
-        this.SelectorY = SelectorYBase + (this.CurrentPosition * SelectorYDiff);
+        if (this.CurrentPosition == 2)
+        {
+            diff = 373;
+            diffY = -50;
+        }
+
+        this.SelectorY = SelectorYBase + (this.CurrentPosition * SelectorYDiff) + diff;
+        this.SelectorX = OgSelectorX + diffY;
     }
 }
