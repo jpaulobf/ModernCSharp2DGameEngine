46cc7061cbfe12aa8210c0777917f32764038186
jpaulobf <jpaulobf@gmail.com>
2023-01-19 14:09:50 -0300

    BitBlt method

--- src/util/BitmapEx.cs
+++ src/util/BitmapEx.cs
@@ -18,4 +18,18 @@ public class BitmapEx
     {
         return new Bitmap(@"" + Launcher.path + filepath);
     }
+
+    public const int SRCCOPY = 0xcc0020; // we want to copy an in memory image
+
+    [System.Runtime.InteropServices.DllImportAttribute("gdi32.dll")]        
+    public static extern int BitBlt(IntPtr hdcDest,     // handle to destination DC (device context)          
+                                     int nXDest,         // x-coord of destination upper-left corner          
+                                     int nYDest,         // y-coord of destination upper-left corner          
+                                     int nWidth,         // width of destination rectangle          
+                                     int nHeight,        // height of destination rectangle          
+                                     IntPtr hdcSrc,      // handle to source DC          
+                                     int nXSrc,          // x-coordinate of source upper-left corner          
+                                     int nYSrc,          // y-coordinate of source upper-left corner
+                                     System.Int32 dwRop  // raster operation code          
+                                     );
 }
