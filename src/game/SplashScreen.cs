0f3c654852dd646e42caf1586038ba27d6a2abe7
jpaulobf <jpaulobf@gmail.com>
2023-01-19 13:23:37 -0300

    Timer adjusted

--- src/game/SplashScreen.cs
+++ src/game/SplashScreen.cs
@@ -175,7 +175,7 @@ public class SplashScreen : Form, ICanvasEngine
     public void Update(long frametime)
     {
         this.Framecounter += frametime;
-        if (this.Framecounter >= 30_000)
+        if (this.Framecounter >= 10_000)
         {
             this.CloseAndGoToGame();
         }
