d3e0e87f67e2a5770f7b523082335eb8a66e1a56
jpaulobf <jpaulobf@gmail.com>
2023-03-05 22:34:54 -0300

    Ending (WIP)

--- src/game/Ending.cs
+++ src/game/Ending.cs
@@ -5,7 +5,9 @@ using Util;
 public class Ending
 {
     private IGame GameRef;
-    private Bitmap TheEnd     = Util.LoadingStuffs.GetInstance().GetImage("the-end");
+    private Bitmap TheEnd = Util.LoadingStuffs.GetInstance().GetImage("the-end");
+    private float X = 0;
+    private float Y = 0;
 
     /**
      * Constructor
@@ -13,10 +15,26 @@ public class Ending
     public Ending(IGame game) 
     {
         this.GameRef = game;
+        if (this.GameRef.WindowSize.Width > this.GameRef.GetInternalResolutionWidth())
+        {
+            this.X = (this.GameRef.WindowSize.Width / 2) - (this.TheEnd.Width * this.GameRef.GetScaleW() / 2);
+            this.Y = (this.GameRef.WindowSize.Height / 2) - (this.TheEnd.Height * this.GameRef.GetScaleH() / 2);
+        }
+        else
+        {
+            this.X = (this.GameRef.GetInternalResolutionWidth() / 2) - (this.TheEnd.Width * this.GameRef.GetScaleW() / 2);
+            this.Y = (this.GameRef.GetInternalResolutionHeight() / 2) - (this.TheEnd.Height * this.GameRef.GetScaleH() / 2);
+        }
+
+        this.X /= this.GameRef.GetScaleW();
+        this.Y -= this.GameRef.GetHUD().GetHudHeight();
+        this.Y /= this.GameRef.GetScaleH();
+
+        Console.WriteLine(X + "  " + Y);
     }
 
     /**
-     * Update the HUD
+     * Update the Ending
      */
     public void Update(long frametime) 
     {
@@ -24,15 +42,24 @@ public class Ending
     }
 
     /**
-     * Draw the HUD
+     * Draw the Ending Stage
      */
     public void Draw(Graphics gfx) 
     {
-        gfx.DrawImage(this.TheEnd, 200, 200, this.TheEnd.Width, this.TheEnd.Height);
+        //draw scenario
+
+        //draw airplane
+
+        //draw adictional sprites
+
+        //draw effects
+
+        //draw ending logo
+        gfx.DrawImage(this.TheEnd, X, Y, this.TheEnd.Width, this.TheEnd.Height);
     }
 
     /**
-     * Reset HUD parameters
+     * Reset Ending parameters
      */
     internal void Reset()
     {
