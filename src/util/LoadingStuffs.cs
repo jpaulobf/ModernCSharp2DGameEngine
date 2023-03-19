503dec6f138571daecc60beb8682a30b6f7185b1
jpaulobf <jpaulobf@gmail.com>
2023-03-19 19:07:58 -0300

    New Stuffs Loaded

--- src/util/LoadingStuffs.cs
+++ src/util/LoadingStuffs.cs
@@ -148,6 +148,9 @@ public class LoadingStuffs
 
             image = BitmapEx.New("img\\end.png");
             Images.Add("the-end", image);
+
+            image = BitmapEx.New("img\\fullscreen_label.png");
+            Images.Add("label-fullscreen", image);
         }
         catch (Exception ex)
         {
