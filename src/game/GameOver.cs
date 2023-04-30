eca351e9eff473281d79c1e4a271167ac50f9cad
jpaulobf <jpaulobf@gmail.com>
2023-04-30 12:02:44 +0100

    GameOver WIP

--- src/game/GameOver.cs
+++ src/game/GameOver.cs
@@ -1,16 +1,31 @@
 namespace Game;
 
+using Util;
+
 public class GameOver
 {
     private IGame GameRef;
     private long Framecounter = 0;
+    private Bitmap GameOverImage;
+    private float GameOverImageX = 0;
+    private float GameOverImageY = 0;
+    private float GameOverImageW = 0;
+    private float GameOverImageH = 0;
 
     /**
      * Constructor
      */
     public GameOver(IGame game) 
     {
-        this.GameRef = game;
+        this.GameRef        = game;
+        this.GameOverImage  = LoadingStuffs.GetInstance().GetImage("gameover");
+        
+        this.GameOverImageW = this.GameOverImage.Width / this.GameRef.GetScaleW();
+        this.GameOverImageH = this.GameOverImage.Height / this.GameRef.GetScaleH();
+
+        this.GameOverImageX = (this.GameRef.GetInternalResolutionWidth() / 2) - (this.GameOverImageW / 2);
+        this.GameOverImageY = (this.GameRef.GetInternalResolutionHeight() / 2) - (this.GameOverImageH / 2) - 30;
+
     }
 
     /**
@@ -31,9 +46,8 @@ public class GameOver
      */
     public void Draw(Graphics gfx) 
     {
-        //gfx.FillRectangle(Brushes.Black, this.SeparatorRect);
-        //gfx.DrawImage(this.FuelMeter, this.FuelMeterX, this.FuelMeterY, this.FuelMeter.Width, this.FuelMeter.Height);
-        //TODO
+        gfx.FillRectangle(Brushes.Black, 0, 0, this.GameRef.GetInternalResolutionWidth(), this.GameRef.GetInternalResolutionHeight());
+        gfx.DrawImage(this.GameOverImage, this.GameOverImageX, this.GameOverImageY, this.GameOverImageW, this.GameOverImageH);
     }
 
     /**
